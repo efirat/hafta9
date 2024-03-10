1ae5ca1834018a001ee2feb716fe2e686eade38a
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-10 18:51:47 +0300

    useEffect 2 - fetch ornegi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://jsonplaceholder.org/users");
+      const jsObjectData = await response.json();
+      setData(jsObjectData);
+    }
+    setTimeout(fetchData, 2000);
+    setTimeout(() => {
+      fetchData();
+    }, 2000);
+  }, []);
+
+  console.log(data);
+
   return (
     <>
-      App
+      { data.length===0 && <p id="loading">Kullanıcı bilgileri yükleniyor, lütfen bekleyin...</p>}
+      {data.map((user) => (
+        <p key={user.id}>{user.firstname}</p>
+      ))}
     </>
   );
 }
